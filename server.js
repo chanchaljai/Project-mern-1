4139ed4faaf8dbdb8f3a5328d29ea81cde2b0a5a
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-30 18:55:11 +0530

    thunder client test register

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import authRoute from "./routes/authRoute.js";
 
 import connectDB from "./config/db.js";
 
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoute);
 
 app.get("/", (req, res) => {
     res.send("API is running");
