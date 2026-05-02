5dffb1789a96da52599166029aa8c252f3488539
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-05-02 05:55:36 +0530

    update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import authRoute from "./routes/authRoute.js";
+import productRoute from "./routes/productRoute.js";
 
 import connectDB from "./config/db.js";
 
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
 
 app.get("/", (req, res) => {
     res.send("API is running");
